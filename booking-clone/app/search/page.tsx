573a4694a7b09bca1f7ba98ad9d3887470d6cfdb
shoeisconfused <uday.shishodia2005@gmail.com>
2024-07-30 13:19:42 -0300

    checkpoint1
    made starter page and nav bar with a search form, started search page.

---
+++ booking-clone/app/search/page.tsx
@@ -0,0 +1,8 @@
+function Searchpage(props) {
+    console.log 
+  return (
+    <div>page</div>
+  )
+}
+
+export default Searchpage
---
+++ booking-clone/data/trending.ts
@@ -0,0 +1,37 @@
+export const Trending_data = [
+    {
+        id: 1,
+        title: "Hawaii",
+        location: "USA",
+        description: "15 deals!",
+        src: "https://www.hiltongrandvacations.com/-/media/images/main/blogs/2020/07/travel-bucket-list-ideas-14-must-see-travel-photo-spots-in-hawaii_header_july_20/image.jpg?h=483&w=724&hash=4A26E8155B519F9480FE8C792F73761A"
+    },
+    {
+        id: 2,
+        title: "Swiss Apls",
+        location: "Switzerland",
+        description: "23 deals!",
+        src: "https://media.worldnomads.com/travel-safety/switzerland/lauterbrunnen-valley-switzerland-gettyimages-1024638132.jpg"
+    },
+    {
+        id: 3,
+        title: "Tokyo",
+        location: "Japan",
+        description: "4 Deals!",
+        src: "https://media.nomadicmatt.com/2024/tokyothings.jpeg"
+    },
+    {
+        id: 4,
+        title: "Dubai",
+        location: "United Arab Emirates",
+        description: "17 deals!",
+        src: "https://lp-cms-production.imgix.net/features/2017/09/dubai-marina-skyline-2c8f1708f2a1.jpg?w=1440&h=810&fit=crop&auto=format&q=75"
+    },
+    {
+        id: 5,
+        title: "Tuscany",
+        location: "Italy",
+        description: "7 deals!",
+        src: "https://images.ctfassets.net/wv75stsetqy3/1yIxeONw62MdYFWceOztWN/6a3615a68d7ed4391a1498bbc3016219/Where_Exactly_is_Tuscany.jpg?q=60&fit=fill&fm=webp"
+    }
+];
